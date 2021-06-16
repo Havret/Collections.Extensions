38f83abe3e318e34dea86f3ad9c9ea05e6e50398
Havret <h4vret@gmail.com>
2021-06-16 22:16:05 +0200

    Implement Minimum Priority Queue

---
+++ src/Collections.Extensions/MinPQ.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Havret.Collections.Extensions
+{
+    public class MinPQ<T> where T : IComparable<T>
+    {
+        private T[] _pq;
+        private int _n;
+
+        /// <param name="capacity">The number of elements that the new queue can initially store.</param>
+        public MinPQ(int capacity)
+        {
+            _pq = new T[capacity + 1];
+        }
+
+        public MinPQ() : this(1)
+        {
+        }
+
+        public void Insert(T key)
+        {
+            // double size of array if necessary
+            if (_n == _pq.Length - 1)
+            {
+                Resize(2 * _pq.Length);
+            }
+
+            // add key, and percolate it up to maintain heap invariant
+            _pq[++_n] = key;
+            Swim(_n);
+        }
+
+        public T DeleteMin()
+        {
+            T min = _pq[1];
+            Exchange(1, _n--);
+            _pq[_n + 1] = default;
+            Sink(1);
+
+            if (_n > 0 && _n == (_pq.Length - 1) / 4)
+            {
+                Resize(_pq.Length / 2);
+            }
+
+            return min;
+        }
+
+        private void Swim(int k)
+        {
+            while (k > 1 && Greater(k / 2, k))
+            {
+                Exchange(k / 2, k);
+                k /= 2;
+            }
+        }
+
+        private void Resize(int capacity)
+        {
+            var temp = new T[capacity];
+            for (int i = 0; i <= _n; i++)
+            {
+                temp[i] = _pq[i];
+            }
+
+            _pq = temp;
+        }
+
+        private void Sink(int k)
+        {
+            while (2 * k <= _n)
+            {
+                int j = 2 * k;
+                if (j < _n && Greater(j, j + 1))
+                {
+                    j++;
+                }
+
+                if (!Greater(k, j))
+                {
+                    break;
+                }
+
+                Exchange(k, j);
+                k = j;
+            }
+        }
+
+        private bool Greater(int i, int j)
+        {
+            return _pq[i].CompareTo(_pq[j]) > 0;
+        }
+
+        private void Exchange(int i, int j)
+        {
+            T temp = _pq[i];
+            _pq[i] = _pq[j];
+            _pq[j] = temp;
+        }
+    }
+}
---
+++ test/Collections.Extensions.Tests/MinPQTest.cs
@@ -0,0 +1,29 @@
+using System;
+using Havret.Collections.Extensions;
+using Xunit;
+
+namespace Collections.Extensions.Tests
+{
+    public class MinPQTest
+    {
+        [Fact]
+        public void should_process_elements_in_ascending_order()
+        {
+            var minPQ = new MinPQ<int>();
+            minPQ.Insert(1);
+            minPQ.Insert(9);
+            minPQ.Insert(2);
+            minPQ.Insert(8);
+            minPQ.Insert(3);
+            minPQ.Insert(7);
+            minPQ.Insert(4);
+            minPQ.Insert(6);
+            minPQ.Insert(5);
+            minPQ.Insert(10);
+            
+            for (int i = 1; i < 11; i++) {
+                Assert.Equal(i, minPQ.DeleteMin());
+            }
+        }
+    }
+}
